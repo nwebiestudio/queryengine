3ea84566d5d0ec71d4d718100765d152d5ca99cf
Vpar7907 <parasochka_va@list.ru>
2022-10-07 15:14:31 +0300

    authorization

--- src/client/src/components/Authorization.tsx
+++ src/client/src/components/Authorization.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Authorization() {
   return (
-    <div>Authorization</div>
+    <div className='flex flex-col justify-center items-center w-screen h-screen'>
+        <form className='flex flex-col justify-center items-center w-[500px] h-[500px] border-2 rounded-xl border-cyan-600'>
+            <input type="login" />
+            <input type="password" />
+            <a href="/">Войти</a>
+        </form>
+    </div>
   )
 }
